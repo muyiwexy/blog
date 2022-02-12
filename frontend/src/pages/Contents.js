48d818d9c90c9b0bfe75b42a4f84ef18c3b3e1a2
Femi-ige Muyiwa <84715593+muyiwexy@users.noreply.github.com>
2022-02-12 13:00:47 +0100

    Create Contents.js

---
+++ frontend/src/pages/Contents.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+import Markdown from 'react-markdown'
+import CodeBlock from '../components/CodeBlock'
+
+export default function ReviewDetails() {
+  const { id } = useParams()
+  const { loading, error, data } = useFetch('http://localhost:1337/api/reviews/' + id)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error :(</p>
+
+  console.log(data)
+
+  return (
+    <div className="review-card">
+      <div className="rating">{data.data.attributes.rating}</div>
+      <h2>{data.data.attributes.title}</h2>
+
+      <small>console list</small>
+
+      <Markdown components={CodeBlock}>{data.data.attributes.body}</Markdown>
+    </div>
+  )
+}
